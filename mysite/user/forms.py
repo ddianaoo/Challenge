a910c0fcc82de09095792d0022b1ef8f66e3acb8
DianaUzun <dianauzun2006@gmail.com>
2023-03-07 21:53:55 +0200

    add static

--- mysite/user/forms.py
+++ mysite/user/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField( widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
     # class Meta:
