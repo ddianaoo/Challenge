5809d64b81b627692dd0d2da952843f8edcc8d18
DianaUzun <dianauzun2006@gmail.com>
2023-03-08 22:04:39 +0200

    add page: add scheme

--- mysite/scheme/forms.py
+++ mysite/scheme/forms.py
@@ -3,6 +3,7 @@ from .models import *
 
 
 class SchemeForm(forms.ModelForm):
+
     class Meta:
         model = Scheme
         fields = ['name', 'user',
@@ -10,3 +11,54 @@ class SchemeForm(forms.ModelForm):
                   'name1', 'name2', 'name3', 'name4', 'name5', 'name6',
                   'order1', 'order2', 'order3', 'order4', 'order5', 'order6',
                   'rows']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:300px'}),
+            'name1': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:300px'}),
+            'name2': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:300px'}),
+            'name3': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:300px'}),
+            'name4': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:300px'}),
+            'name5': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:300px'}),
+            'name6': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:300px'}),
+            'type1': forms.Select(attrs={'class': 'form-select', 'style': 'width:300px'}),
+            'type2': forms.Select(attrs={'class': 'form-select', 'style': 'width:300px'}),
+            'type3': forms.Select(attrs={'class': 'form-select', 'style': 'width:300px'}),
+            'type4': forms.Select(attrs={'class': 'form-select', 'style': 'width:300px'}),
+            'type5': forms.Select(attrs={'class': 'form-select', 'style': 'width:300px'}),
+            'type6': forms.Select(attrs={'class': 'form-select', 'style': 'width:300px'}),
+            'order1': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:200px'}),
+            'order2': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:200px'}),
+            'order3': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:200px'}),
+            'order4': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:200px'}),
+            'order5': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:200px'}),
+            'order6': forms.TextInput(attrs={'class': 'form-control', 'style': 'width:200px'}),
+        }
+        labels = {
+            'name':  'Name',
+            'name1': 'Column name',
+            'name2': 'Column name',
+            'name3': 'Column name',
+            'name4': 'Column name',
+            'name5': 'Column name',
+            'name6': 'Column name',
+            'type1': 'Type',
+            'type2': 'Type',
+            'type3': 'Type',
+            'type4': 'Type',
+            'type5': 'Type',
+            'type6': 'Type',
+            'order1': 'Order',
+            'order2': 'Order',
+            'order3': 'Order',
+            'order4': 'Order',
+            'order5': 'Order',
+            'order6': 'Order',
+        }
+        choices = {
+            'type1': CHOICES,
+            'type2': CHOICES,
+            'type3': CHOICES,
+            'type4': CHOICES,
+            'type5': CHOICES,
+            'type6': CHOICES,
+
+        }
--- mysite/scheme/views.py
+++ mysite/scheme/views.py
@@ -8,7 +8,7 @@ from django.views.generic import ListView
 def get_schemas(request):
     if request.user.is_authenticated:
         user = request.user
-        queryset = Scheme.objects.filter(user=user)
+        queryset = Scheme.objects.filter(user=user).order_by('-id')
         return render(request, "scheme/get_schemas.html", {"object_list": queryset})
     return redirect('signin')
 
@@ -16,13 +16,12 @@ def get_schemas(request):
 def add_scheme(request):
     user = request.user
     user = user.id
-
     if request.method == 'POST':
         form = SchemeForm(request.POST, initial={'user': user})
         if form.is_valid():
             form.save()
             return redirect('schemas')
-
+    # scheme_form
     return render(request, 'scheme/add_scheme.html', {'form': SchemeForm(initial={'user': user}), })
 
 
