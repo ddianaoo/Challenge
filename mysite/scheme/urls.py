7321ef45798ba466f9b3e236900819fdaf51969c
DianaUzun <dianauzun2006@gmail.com>
2023-03-11 19:49:30 +0200

    improve single scheme page(with data generating)

--- mysite/scheme/urls.py
+++ mysite/scheme/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('all/', login_required(get_schemas), name='schemas'),
     path('add/', login_required(add_scheme), name='add_scheme'),
     path('delete/<int:pk>/', login_required(delete_scheme), name='delete_scheme'),
-    path('details/<int:pk>/', login_required(single_scheme), name='single_scheme'),
-    path('details-and-gen/<int:pk>/', login_required(single_generate), name='single_generate'),
-    path('details/<int:pk>/data-sets/create/', login_required(create_set), name='create_data_sets'),
+    path('details/<int:pk>/', login_required(single_generate), name='single_scheme'),
 ]
